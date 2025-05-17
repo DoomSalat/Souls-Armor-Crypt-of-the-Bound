4cbf9a9c713811d59240ff26c9dc49b50988fdf3
Capitan Salat <kd.zaytcev@gmail.com>
2025-05-17 14:15:41 +0300

    Создана более контролируемая тряска меча

---
+++ Assets/Content/Player knight/Scripts/AngularShake.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class AngularShake : MonoBehaviour
+{
+	[SerializeField] private float _strength = 15f; // Максимальный угол тряски (градусы)
+	[SerializeField] private float _duration = 0.5f; // Длительность одной тряски
+	[SerializeField] private int _vibrato = 10;     // Кол-во "вибраций" за _duration
+	[SerializeField] private float _randomness = 90f; // Расброс направления в градусах
+	[SerializeField] private bool _fadeOut = true;
+
+	private Tween _shakeTween;
+
+	private void Start()
+	{
+		Play();
+	}
+
+	private void OnDestroy()
+	{
+		Stop();
+	}
+
+	[ContextMenu(nameof(Play))]
+	public void Play()
+	{
+		_shakeTween?.Kill();
+		_shakeTween = transform.DOShakeRotation(_duration, new Vector3(0, 0, _strength), _vibrato, _randomness, _fadeOut)
+			.SetEase(Ease.Linear)
+			.SetLoops(-1);
+	}
+
+	[ContextMenu(nameof(Stop))]
+	public void Stop()
+	{
+		_shakeTween?.Kill();
+		transform.rotation = Quaternion.identity;
+	}
+}
